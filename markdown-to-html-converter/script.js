0e8636c88d95cf50ae4974216f4e9e91c764005c
klhaug <kristian@snougle.no>
2025-04-26 08:23:42 +0200

    testing function done. Convert-logic needs work

--- markdown-to-html-converter/script.js
+++ markdown-to-html-converter/script.js
@@ -4,14 +4,62 @@ const preview = document.getElementById("preview");
 
 markdownInput.addEventListener("input", convertMarkDown)
 
-const regex = /(?:^|\s)[#]{1,3}\s/i;
+const h1Reg = /(?:^|\s)[#]{1}\s/i;
+const h2Reg = /(?:^|\s)[#]{2}\s/i;
+const h3Reg = /(?:^|\s)[#]{3}\s/i;
+
+const regexArray = [h1Reg, h2Reg, h3Reg];
+const markupArray = [
+  {
+    open: "<h1>",
+    close:"</h1>"
+  },
+  {
+    open: "<h2>",
+    close: "</h2>"
+  },
+  {
+    open: "<h3>",
+    close: "</h3>"
+  }
+];
 
 
-
-function convertMarkDown(){
-  console.log(markdownInput.value)
-  
+//Checks if anythings matches the different regexes, if so, returns the index of the matched regex.
+function testRegex(input) {
+  let i;
+  const isMatch = regexArray.some((regex, index) => {
+    i = index;
+    return regex.test(input);
+    }
+  )
+  if(isMatch){
+    return i;
+  } else {
+    return "No match found"
+  }
 }
 
 
-console.log(regex.test("# "))
+function convertMarkDown(){
+  const input = markdownInput.value
+  const regexNumber = testRegex(input);
+
+  preview.innerHTML = input;
+  htmlOutput.textContent = input;
+  
+  //TODO: Find a solution to how to place the closing tag, and how to make it stay after a linebreak.
+
+  if(typeof regexNumber === "number"){
+    const replaceRegex = input.replace(regexArray[regexNumber], markupArray[regexNumber].open) + markupArray[regexNumber].close
+    preview.innerHTML = replaceRegex;
+    htmlOutput.textContent = replaceRegex;
+  }
+}
+
+
+
+
+//console.log(regex.test("# "))
+//console.log(convert("##"))
+//console.log(h1Reg.test("# "))
