d1f79247645fbe3b7884b0cb51c096ffba0be128
klhaug <kristian@snougle.no>
2025-04-28 07:09:41 +0200

    mvp of markdownconverter complete

--- markdown-to-html-converter/script.js
+++ markdown-to-html-converter/script.js
@@ -3,19 +3,37 @@ const htmlOutput = document.getElementById("html-output");
 const preview = document.getElementById("preview");
 
 markdownInput.addEventListener("input", convertMarkdown)
-
-//TODO -> Nå er du på riktig spor. Bare skriv resten av regexene, for så å legge inn en replace all på alt. Så kjør en code-review m. GPT. 
-
+//
 const h1Reg = /^ *#{1} (.+)/igm;
 const h2Reg = /^ *#{2} (.+)/igm;
 const h3Reg = /^ *#{3} (.+)/igm;
+const boldReg = /[*]{2}(.+)[*]{2}/ig;
+const boldReg2 = /[_]{2}(.+)[_]{2}/ig;
+const italicReg = /[*]{1}(.+)[*]{1}/ig;
+const italicReg2 = /[_]{1}(.+)[_]{1}/ig;
+const imgReg = /!\[([^\]]+)\]\(([^\)]+)\)/ig;
+const linkReg = /\[([^\]]+)\]\(([^\)]+)\)/ig;
+const quoteReg = /(?<!.+)\> (.+)/ig;
 
+
+console.log(quoteReg.test("this > this "))
+console.log("![hello](img-source)".match(imgReg))
+
+//På sikt, kanskje refactor denne så du ikke skriver replaceAll så mange ganger... 
 
 function convertMarkdown(){
   const input = markdownInput.value
     .replaceAll(h1Reg, "<h1>$1</h1>")
     .replaceAll(h2Reg, "<h2>$1</h2>")
     .replaceAll(h3Reg, "<h3>$1</h3>")
+    .replaceAll(boldReg, "<strong>$1</strong>")
+    .replaceAll(boldReg2, "<strong>$1</strong>")
+    .replaceAll(italicReg, "<em>$1</em>")
+    .replaceAll(italicReg2, "<em>$1</em>")
+    .replaceAll(imgReg,`<img alt="$1" src="$2">`)
+    .replaceAll(linkReg,`<a href="$2">$1</a>`)
+    .replaceAll(quoteReg, "<blockquote>$1</blockquote>")
+
 
 
   preview.innerHTML = input;
@@ -23,3 +41,4 @@ function convertMarkdown(){
 
   return input;
 }
+
